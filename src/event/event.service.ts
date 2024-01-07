7466c99d634ca3db52a504e70df7c02f499f5d71
Pavlo Horbatiuk <pavlohorbatiuk@gmail.com>
2024-01-07 12:38:54 +0200

    added create event

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,14 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class EventService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
-  create(createEventDto: CreateEventDto) {
-    return  this.prisma.event.create({data: createEventDto})
+  async create(createEventDto: CreateEventDto) {
+    const userExists = await this.prisma.exists(this.prisma.user,{
+      id: createEventDto.authorId,
+    })
+    
+    const isSameTime = await this.prisma.exists(this.prisma.event,{date:createEventDto.date})
+
+    if(isSameTime){
+      throw new NotFoundException("in this date and time already lessons exists")
+    }
+    if (userExists) {
+      return this.prisma.event.create({ data: createEventDto })
+    } else {
+      throw new NotFoundException("User dosen't exists")
+    }
   }
 
   findAll() {
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { ExtendsHook, DefaultArgs, InternalArgs } from '@prisma/client/runtime/library';
 
 @Injectable()
-export class PrismaService extends PrismaClient {}
+export class PrismaService extends PrismaClient {
+    constructor() {
+        super();
+      }
+
+      async exists<T>(
+        model: T,
+        where: Prisma.Args<T, 'findFirst'>['where']
+      ): Promise<boolean> {
+        const context = Prisma.getExtensionContext(model);
+    
+        const result = await (context as any).findFirst({ where });
+        return result !== null;
+      }
+}
