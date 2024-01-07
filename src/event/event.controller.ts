257b45390990c3563abf41b247b36f28bb2ae33e
Vitalik0422 <vitaliy19980422@gmail.com>
2024-01-07 23:28:41 +0200

    Vitalik

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Logger} from '@nestjs/common';
 import { EventService } from './event.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { findOneEventDto } from './dto/findOne-event.dto'
 import { ApiCreatedResponse } from '@nestjs/swagger';
 import { EventEntity } from './entities/event.entity';
 
@@ -15,14 +16,14 @@ export class EventController {
     return this.eventService.create(createEventDto);
   }
 
-  @Get()
-  findAll() {
-    return this.eventService.findAll();
+  @Get('/getall')
+  findAll(@Param('id') id: number) {
+    return this.eventService.findAll(id);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.eventService.findOne(+id);
+  @Post('/findOne')
+  findOne(@Body() findOneEventDto: findOneEventDto) {
+    return this.eventService.findOne(findOneEventDto);
   }
 
   @Patch(':id')
