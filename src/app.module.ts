c4dd693e7e09780ad8eda5d55928eb3aa2443698
Pavlo Horbatiuk <pavlohorbatiuk@gmail.com>
2024-01-10 11:15:07 +0200

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { EventModule } from './event/event.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-	imports: [AuthModule, PrismaModule, EventModule],
+	imports: [AuthModule, PrismaModule, EventModule, ConfigModule.forRoot({envFilePath:'.env'})],
 	controllers: [],
 	providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -17,8 +17,8 @@ async function bootstrap() {
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('api', app, document);
 	app.enableCors();
-
-	await app.listen(3001);
+	const PORT = process.env.PORT || 5000;
+	await app.listen(PORT);
 
 	if (module.hot) {
 		module.hot.accept();
