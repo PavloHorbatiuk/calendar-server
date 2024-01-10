ad48e9d441c7808e3937af485a9254d43fe979cd
Pavlo Horbatiuk <pavlohorbatiuk@gmail.com>
2024-01-10 11:07:04 +0200

    update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from 'src/strategy';
 
 export const jwtSecret = 'zjP9i6ZS5LoSKCRk';
 
@@ -18,6 +19,6 @@ export const jwtSecret = 'zjP9i6ZS5LoSKCRk';
 		}),
 	],
 	controllers: [AuthController],
-	providers: [AuthService],
+	providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
